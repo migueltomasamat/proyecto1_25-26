40ae5322390c88f7abfd01cf9963a1fcca4614ff
Miguel Angel <matomas@localhost>
2025-09-12 20:39:24 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Ejercicios DWES</title>
+</head>
+<body>
+
+    <h1>Ejercicios de Desarrollo Web en Entorno Servidor</h1>
+
+    <ol>
+        <li><a href="ejercicios/hola-mundo.php">Hola Mundo</a></li>
+        <li><a href="ejercicios/bucle.php">Bucle FOR</a></li>
+
+    </ol>
+
+</body>
+
+</html>
