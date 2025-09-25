a00305d1384f8f0b8b7c247035930fb271746a80
matomas <migueltoma.informatica@iespacomolla.es>
2025-09-25 20:27:17 +0200

    Añadidas vistas

---
+++ src/admin/views/generarpass.php
@@ -0,0 +1,12 @@
+<?php
+$title="Generador";
+
+include_once "template/head.php";
+include_once "template/header.php";
+include_once "template/aside.php";
+$tituloSeccion="Contraseña Generada";
+include_once "template/main.php";
+
+echo "asdfasjkdfhajkfgsdhcfasduyv 3iopr423 4";
+
+include_once "template/footer.php";
---
+++ src/admin/views/juegos.php
@@ -0,0 +1,9 @@
+<?php
+$title="Administración de Netflix";
+
+include_once "template/head.php";
+include_once "template/header.php";
+include_once "template/aside.php";
+$tituloSeccion="Juegos";
+include_once "template/main.php";
+include_once "template/footer.php";
